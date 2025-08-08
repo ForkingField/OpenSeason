66e350482ff6dc3216cd978fff18d5d112fd0178
xiota <github@mentalfossa.com>
2025-08-08 19:06:46 +0000

    build(cmake): remove detect_compiler and warnings

--- CMakeModules/DuckStationBuildSummary.cmake
+++ CMakeModules/DuckStationBuildSummary.cmake
@@ -29,23 +29,3 @@ if(ALLOW_INSTALL)
   ${CMAKE_INSTALL_PREFIX}
 It does **not** use the LSB subdirectories of bin, share, etc, so you should disable this option if it is set to /usr or /usr/local.")
 endif()
-
-if(NOT IS_SUPPORTED_COMPILER)
-  message(WARNING "
-*************** UNSUPPORTED CONFIGURATION ***************
-You are not compiling DuckStation with a supported compiler.
-It may not even build successfully.
-DuckStation only supports the Clang and MSVC compilers.
-No support will be provided, continue at your own risk.
-*********************************************************")
-endif()
-
-if(WIN32)
-  message(WARNING "
-*************** UNSUPPORTED CONFIGURATION ***************
-You are compiling DuckStation with CMake on Windows.
-It may not even build successfully.
-DuckStation only supports MSBuild on Windows.
-No support will be provided, continue at your own risk.
-*********************************************************")
-endif()
