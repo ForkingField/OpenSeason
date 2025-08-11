f3c3faa84ebc2ee94d108a9e26249fd771cf1b19
xiota <github@mentalfossa.com>
2025-08-11 20:37:50 +0000

    chore(cmake): fix libbacktrace variable usage

--- CMakeModules/FindLibbacktrace.cmake
+++ CMakeModules/FindLibbacktrace.cmake
@@ -1,16 +1,15 @@
 # - Try to find libbacktrace
-# Once done this will define
+# Once done this will define:
 #  LIBBACKTRACE_FOUND - System has libbacktrace
 #  LIBBACKTRACE_INCLUDE_DIRS - The libbacktrace include directories
 #  LIBBACKTRACE_LIBRARIES - The libraries needed to use libbacktrace
 
-FIND_PATH(
+find_path(
     LIBBACKTRACE_INCLUDE_DIR backtrace.h
-    HINTS /usr/include /usr/local/include
     ${LIBBACKTRACE_PATH_INCLUDES}
 )
 
-FIND_LIBRARY(
+find_library(
     LIBBACKTRACE_LIBRARY
     NAMES backtrace
     PATHS ${ADDITIONAL_LIBRARY_PATHS} ${LIBBACKTRACE_PATH_LIB}
